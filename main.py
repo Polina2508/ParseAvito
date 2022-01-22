84e9ea02c4bb554cacbcd349d5b19022b991f825
Polina <ptomasevskaa30@gmail.com>
2022-01-23 01:29:22 +0300

    second commit

--- main.py
+++ main.py
@@ -29,35 +29,36 @@ CON = psycopg2.connect(
 
 
 class Parser:   
-    """"""
+    """Countind the number of pages"""
     
-    # @staticmethod
-    # async def main(url, params=None):
+    @staticmethod
+    async def main(url, params=None):
 
-    #     async with aiohttp.ClientSession(headers={'Referer': 'https://www.avito.ru'}) as session:
-    #         async with session.get(url) as response:
-    #             print("Status:", response.status)
-    #             print("Content-type:", response.headers['content-type'])
+        async with aiohttp.ClientSession(headers={'Referer': URL }) as session:
+            async with session.get(url) as response:
+                print("Status:", response.status)
+                print("Content-type:", response.headers['content-type'])
 
-    #             html = await response.text()
-    #             # print(html)
-    #             print("Body:", html[:15], "...")
+                html = await response.text()
+                # print(html)
+                print("Body:", html[:15], "...")
             
-    #     return response
-            # loop = asyncio.get_event_loop()         return loop.run_until_complete(await Parser.main(url, params=None))
+        
+        loop = asyncio.get_event_loop()        
+        return loop.run_until_complete(await Parser.main(url, params=None))
 
 
 
     
     @staticmethod
     async def get_html(url, params=None):
-        """"""
+        """get html"""
         
         return requests.get(url, headers=HEADERS, params=params)
 
     @staticmethod
     async def pagination_pag(html):
-        """"""
+        """Countind the number of pages"""
 
         soup = BeautifulSoup(html, 'html.parser')
         pagination = soup.find_all('span', class_='pagination-item-JJq_j')
@@ -69,7 +70,7 @@ class Parser:
 
     @staticmethod
     async def get_content(html):
-        """dgjsh"""
+        """get the links"""
 
         soup = BeautifulSoup(html, 'html.parser')
         items = soup.find_all('a', class_ = 'link-link-MbQDP link-design-default-_nSbv title-root-zZCwT iva-item-title-py3i_ title-listRedesign-_rejR title-root_maxHeight-X6PsH')
@@ -83,7 +84,7 @@ class Parser:
     
     @staticmethod
     async def get_info(urls):
-        """"""
+        """parse information from links"""
 
         for url in urls:
             data = await Parser.get_html(url)
@@ -95,7 +96,7 @@ class Parser:
 
     @staticmethod
     async def save_db(title, price):
-        """"""
+        """save to database"""
         
         CON.cursor().execute(
             """INSERT INTO hous (title, price) VALUES
@@ -114,18 +115,18 @@ class Parser:
     #             writer.writerow([item['title'], item['price']])  
     
     async def parse(self):
-        """"""
+        
         
         html = await self.get_html(URL)
         if html.status_code == 200:
-            # houses = []
+            
             pages_count = await self.pagination_pag(html.text)
             
             for page in range(1, pages_count + 1):
                 print(f'Парсинг страницы {page} из {pages_count}')
                 html = await self.get_html(URL, params={'page': page})
                 await self.get_content(html.text)
-            # save_file(houses, FILE)
+            
             # print(f'Получено {len(houses)} предложений')
         else:
             print('ERROR')
